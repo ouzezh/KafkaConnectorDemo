e2d80518eb1b50181406f2aaa7482a52b8819f09
ouzezhou <ouzezhou@xdf.cn>
2019-01-22 15:11:39 +0800

    source offset adapter

--- src/main/java/com/ozz/kafka/connector/source/offsetreader/FileOffsetAdapter.java
+++ src/main/java/com/ozz/kafka/connector/source/offsetreader/FileOffsetAdapter.java
@@ -8,12 +8,13 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.TimeUnit;
-
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigDef.Importance;
 import org.apache.kafka.common.config.ConfigDef.Type;
+import org.apache.kafka.connect.data.SchemaAndValue;
 import org.apache.kafka.connect.json.JsonConverter;
 import org.apache.kafka.connect.runtime.WorkerConfig;
 import org.apache.kafka.connect.runtime.standalone.StandaloneConfig;
@@ -21,13 +22,12 @@ import org.apache.kafka.connect.storage.Converter;
 import org.apache.kafka.connect.storage.FileOffsetBackingStore;
 import org.apache.kafka.connect.storage.OffsetStorageReaderImpl;
 import org.apache.kafka.connect.util.FutureCallback;
-
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class FileOffsetAdapter implements Closeable {
   private OffsetStorageReaderImpl offsetReader;
-  private FileOffsetBackingStore store;
+  private FileOffsetBackingStoreAdapter offsetStore;
   private String namespace;
 
   private Converter keyConverter;
@@ -38,34 +38,31 @@ public class FileOffsetAdapter implements Closeable {
     String file = "C:\\Users\\ouzezhou\\Desktop\\connect.offsets";
 
     ObjectMapper objectMapper = new ObjectMapper();
-    Map<String, String> partition = objectMapper.readValue("{\"db\":\"BJNIS2\",\"table\":\"test_bs_lesson\"}", new TypeReference<HashMap<String, String>>() {});
-    Map<String, String> partition2 = objectMapper.readValue("{\"db\":\"BJNIS2\",\"table\":\"xxx\"}", new TypeReference<HashMap<String, String>>() {});
 
-    try (FileOffsetAdapter reader = new FileOffsetAdapter(connectorName, file);) {
-      // read old offset
-      Map<String, Object> offset = reader.offset(partition);
-      System.out.println(offset);
-
-      // read new offset
-      System.out.println(reader.offset(partition2));
+    try (FileOffsetAdapter adapter = new FileOffsetAdapter(connectorName, file);) {
+      // read offsets
+      Map<Map<String, Object>, Map<String, Object>> offsets = adapter.offsets();
+      System.out.println(String.format("before put: %d", offsets.size()));
+      for (Entry<Map<String, Object>, Map<String, Object>> en : offsets.entrySet()) {
+        System.out.println(String.format("%s = %s", en.getKey(), en.getValue()));
+      }
 
       // put offset
+      Map<String, String> partition = objectMapper.readValue("{\"topic\":\"connect-test\",\"file\":\"xxx\"}", new TypeReference<HashMap<String, String>>() {});
       List<Pair<Map<String, String>, Map<String, Long>>> list = new ArrayList<>();
-      Map<String, Long> offset2 = objectMapper.readValue(String.format("{\"lastUpdateVersion\":%s,\"lastLoadId\":%s}",
-                                                                       System.currentTimeMillis(),
-                                                                       System.currentTimeMillis()),
-                                                         new TypeReference<HashMap<String, Long>>() {});
-      list.add(Pair.of(partition2, offset2));
-      reader.put(list);
+      Map<String, Long> offset = objectMapper.readValue(String.format("{\"position\":%s}", System.currentTimeMillis()),
+                                                        new TypeReference<HashMap<String, Long>>() {});
+      list.add(Pair.of(partition, offset));
+      adapter.put(list);
     }
 
-    try (FileOffsetAdapter reader = new FileOffsetAdapter(connectorName, file);) {
-      // read old offset
-      Map<String, Object> offset = reader.offset(partition);
-      System.out.println(offset);
-
-      // read new offset
-      System.out.println(reader.offset(partition2));
+    try (FileOffsetAdapter adapter = new FileOffsetAdapter(connectorName, file);) {
+      // read offsets
+      Map<Map<String, Object>, Map<String, Object>> offsets = adapter.offsets();
+      System.out.println(String.format("after put: %d", offsets.size()));
+      for (Entry<Map<String, Object>, Map<String, Object>> en : offsets.entrySet()) {
+        System.out.println(String.format("%s = %s", en.getKey(), en.getValue()));
+      }
     }
   }
 
@@ -77,9 +74,9 @@ public class FileOffsetAdapter implements Closeable {
     props.put(StandaloneConfig.OFFSET_STORAGE_FILE_FILENAME_CONFIG, file);
     WorkerConfig config = new WorkerConfig(def, props);
 
-    store = new FileOffsetBackingStore();
-    store.configure(config);
-    store.start();
+    offsetStore = new FileOffsetBackingStoreAdapter();
+    offsetStore.configure(config);
+    offsetStore.start();
 
     Map<String, Object> arg0 = Collections.singletonMap("schemas.enable", Boolean.FALSE);
     keyConverter = new JsonConverter();
@@ -87,11 +84,28 @@ public class FileOffsetAdapter implements Closeable {
     valueConverter = new JsonConverter();
     valueConverter.configure(arg0, false);
 
-    offsetReader = new OffsetStorageReaderImpl(store, connectorName, keyConverter, valueConverter);
+    offsetReader = new OffsetStorageReaderImpl(offsetStore, connectorName, keyConverter, valueConverter);
+  }
+
+  public class FileOffsetBackingStoreAdapter extends FileOffsetBackingStore {
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public <T> List<Map<String, T>> partitions() {
+      List<Map<String, T>> list = new ArrayList<>();
+      for (ByteBuffer key : data.keySet()) {
+        SchemaAndValue sv = keyConverter.toConnectData(namespace, key.array());
+        List arr = (List) sv.value();
+        list.add((Map<String, T>) arr.get(1));
+      }
+      return list;
+    }
   }
 
   public <T> Map<String, Object> offset(Map<String, T> partition) {
     return offsetReader.offset(partition);
+  }
+
+  public <T> Map<Map<String, T>, Map<String, Object>> offsets() {
+    return offsetReader.offsets(offsetStore.partitions());
   }
 
   public <T, U> void put(List<Pair<Map<String, T>, Map<String, U>>> list) {
@@ -103,7 +117,7 @@ public class FileOffsetAdapter implements Closeable {
     }
 
     FutureCallback<Void> callback = new FutureCallback<>();
-    store.set(values, callback);
+    offsetStore.set(values, callback);
 
     try {
       callback.get(10, TimeUnit.SECONDS);
@@ -122,7 +136,6 @@ public class FileOffsetAdapter implements Closeable {
 
   @Override
   public void close() {
-    store.stop();
+    offsetStore.stop();
   }
-
 }
