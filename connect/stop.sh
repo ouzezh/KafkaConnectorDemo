8aa58efc35dcdebc2371533325f73d4bc4c2e088
ouzezhou <ouzezhou@xdf.cn>
2019-04-03 10:28:20 +0800

    stop shell

--- connect/stop.sh
+++ connect/stop.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
 
 curl localhost:8083/connectors | jq
-curl -X DELETE --connect-timeout 30 http://localhost:8083/connectors/local-file-source && echo "stop source success" || echo "stop source fail"
-curl -X DELETE --connect-timeout 30 http://localhost:8083/connectors/local-file-sink && echo "stop sink success" || echo "stop sink fail"
+curl -X DELETE --connect-timeout 15 http://localhost:8083/connectors/local-file-source && echo "stop source success" || echo "stop source fail"
+curl -X DELETE --connect-timeout 15 http://localhost:8083/connectors/local-file-sink && echo "stop sink success" || echo "stop sink fail"
 curl localhost:8083/connectors | jq
 
 dir=$(cd $(dirname $0);pwd)
